62a97378bad21e81560fe4a744c6477db8b74dfb
Steve Dillon <stevedillon001@gmail.com>
2017-04-02 13:29:44 -0700

    completed checkpoint9

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -103,10 +103,96 @@
     puts "New entry created"
   end
 
-  def search_entries
+  def edit_entry(entry)
+    # #4 we perform a series of print statements followed by gets.chomp assignment statements. Each gets.chomp statement gathers user input and assigns it to an appropriately named variable.
+    print "Updated name: "
+    name = gets.chomp
+    print "Updated phone number: "
+    phone_number = gets.chomp
+    print "Updated email: "
+    email = gets.chomp
+    # #5 we use !attribute.empty? to set attributes on entry only if a valid attribute was read from user input.
+    entry.name = name if !name.empty?
+    entry.phone_number = phone_number if !phone_number.empty?
+    entry.email = email if !email.empty?
+    system "clear"
+    # #6 we print out entry with the updated attributes.
+    puts "Updated entry:"
+    puts entry
   end
 
+  # We remove entry from address_book and print out a message to the user that says  entry has been removed
+  def delete_entry(entry)
+    address_book.entries.delete(entry)
+    puts "#{entry.name} has been deleted"
+  end
+
+  def search_entries
+    # #9 we get the name that the user wants to search for and store it in name
+    print "Search by name: "
+    name = gets.chomp
+    # #10 we call search on address_book which will either return a match or nil, it will never return an empty string since import_from_csv will fail if an entry does not have a name.
+    match = address_book.binary_search(name)
+    system "clear"
+    # #11 we check if search returned a match. This expression evaluates to false if search returns nil since nil evaluates to false in Ruby. If search finds a match then we call a helper method called search_submenu
+    if match
+      puts match.to_s
+      search_submenu(match)
+    else
+      puts "No match found for #{name}"
+    end
+  end
+
+  def search_submenu(entry)
+    # #12 we print out the submenu for an entry
+    puts "\nd - delete entry"
+    puts "e - edit this entry"
+    puts "m - return to main menu"
+    # #13 we save the user input to selection
+    selection = gets.chomp
+ 
+    # #14 we use a case statement and take a specific action based on user input. If the user input is d we call delete_entry and after it returns we call main_menu. If the input is e we call edit_entry. m will return the user to the main menu. If the input does not match anything (see the else statement) then we clear the screen and ask for their input again by calling search_submenu
+    case selection
+    when "d"
+      system "clear"
+      delete_entry(entry)
+      main_menu
+    when "e"
+      edit_entry(entry)
+      system "clear"
+      main_menu
+    when "m"
+      system "clear"
+      main_menu
+    else
+      system "clear"
+      puts "#{selection} is not a valid input"
+      puts entry.to_s
+      search_submenu(entry)
+    end
+   end
+
   def read_csv
+    # #1 we prompt the user to enter a name of a CSV file to import. We get the filename from STDIN and call the chomp method which removes newlines
+    print "Enter CSV file to import: "
+    file_name = gets.chomp
+
+    # #2 we check to see if the file name is empty. If it is then we return the user back to the main menu by calling main_menu
+    if file_name.empty?
+       system "clear"
+       puts "No CSV file read"
+       main_menu
+    end
+
+    # #3 we import the specified file with import_from_csv on address_book. We then clear the screen and print the number of entries that were read from the file. All of these commands are wrapped in a begin/rescue block. begin will protect the program from crashing if an exception is thrown.
+    begin
+      entry_count = address_book.import_from_csv(file_name).count
+      system "clear"
+      puts "#{entry_count} new entries added from #{file_name}"
+    rescue
+      puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+      read_csv
+    end
   end
 
   def entry_submenu(entry)
@@ -122,7 +208,12 @@
     when "n"
     # #19 we'll handle deleting and editing in another checkpoint, for now theuser will be shown the next entry
     when "d"
+    # #7 when a user is viewing the submenu and they press d, we call delete_entry. 
+      delete_entry(entry)
     when "e"
+    # #8 we call edit_entry when a user presses e. We then display a sub-menu with  entry_submenu for the entry under edit.
+      edit_entry(entry)
+      entry_submenu(entry)
     # #20 we return the user to the main menu
     when "m"
       system "clear"
